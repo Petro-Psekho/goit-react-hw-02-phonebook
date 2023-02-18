0508f7a134496d3997cae7e29e88cf9f68a8cf70
Petro Psekho <psekho.pp@gmail.com>
2023-02-18 18:56:33 +0200

    on progress

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 
 import { Container } from 'components/App.styled';
-import { ContactForm } from 'components/ContactForm/ContactForm';
+import ContactForm from 'components/ContactForm/ContactForm';
 
 class App extends Component {
   state = {
     contacts: [],
-    name: '',
+    // name: '',
+  };
+
+  formSubmitData = data => {
+    console.log(data);
   };
 
   render() {
     return (
       <Container>
         <h1>Phonebook</h1>
-        <ContactForm />
+        <ContactForm onSubmit={this.handleSubmit} />
         <h2>Contacts</h2>
       </Container>
     );
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,3 +1,5 @@
+import React, { Component } from 'react';
+
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as yup from 'yup';
 import { ErrMessage } from 'components/ContactForm/ContactForm.styled';
@@ -6,44 +8,69 @@ const pattern = "^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-
 const title =
   "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan";
 
-const initialValues = {
-  name: '',
-};
-
 const validationSchema = yup.object({
   name: yup.string().required().matches(pattern, title),
 });
 
-export const ContactForm = () => {
-  const handleSubmit = (values, { resetForm }) => {
-    console.log(values);
+class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleSubmit = (values, { resetForm }) => {
+    console.log(values.name);
+    console.log(values.number);
+
+    this.setState(values);
 
     resetForm();
   };
 
-  return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={validationSchema}
-      onSubmit={handleSubmit}
-    >
-      <Form>
-        <label htmlFor="name">
-          Name
-          <Field
-            autoComplete="off"
-            type="text"
-            name="name"
-            // pattern={pattern}
-            // title={title}
-            // required
-          />
-        </label>
-        <button type="submit">Add contact</button>
-        <ErrMessage>
-          <ErrorMessage name="name" />
-        </ErrMessage>
-      </Form>
-    </Formik>
-  );
-};
+  render() {
+    const { name } = this.state;
+
+    return (
+      <Formik
+        initialValues={{ name }}
+        validationSchema={validationSchema}
+        onSubmit={this.handleSubmit}
+      >
+        <Form>
+          <label>
+            Name
+            <Field
+              autoComplete="off"
+              type="text"
+              name="name"
+              // value={this.state.value}
+
+              // pattern={pattern}
+              // title={title}
+              // required
+            />
+          </label>
+          <ErrMessage>
+            <ErrorMessage name="name" />
+          </ErrMessage>
+          <label>
+            Number
+            <Field
+              autoComplete="off"
+              type="tel"
+              name="number"
+              // value={this.state.value}
+              // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              // title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              // required
+            />
+          </label>
+          <ErrorMessage name="number" component="div" />
+          <button type="submit">Add contact</button>
+        </Form>
+      </Formik>
+    );
+  }
+}
+
+export default ContactForm;
